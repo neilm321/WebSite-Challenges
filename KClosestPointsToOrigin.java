d05357875196587d88c7dbab18ab0b657e015c5a
neilm321 <neil.m321@outlook.com>
2021-12-21 20:49:15 +0000

    Create KClosestPointsToOrigin.java

---
+++ KClosestPointsToOrigin.java
@@ -0,0 +1,19 @@
+class Solution {
+  public int[][] kClosest(int[][] points, int k){
+    
+    int[][] ans = new int [k][2];
+    int distances[] = new int[points.length];
+    for (int i=0; i < points.length];
+         for (int i = 0 < points.length; i++)
+         {
+           distances[i] = points[i][0] * points[i][0] + points[j][1] * points[j][1];
+           if (temp <= max)
+           {
+             ans[count][0] = points[j][0];
+             ans[count][1] = points[j][1];
+             count++;
+           }
+         }
+         return ans;
+         }
+         }
